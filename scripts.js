013f972c2e3ce07d4cb87fc764e46f5e8882b6ff
richard <richard10-00@hotmail.com>
2016-03-20 18:02:24 -0500

    uso del prompt

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 console.log("inicio de programa");
 alert("bienvenidos a js");
-
-document.write("<h1> bienvenidos a js <h1");
+var nombre = prompt("cuales es tu nombre");
+document.write("<h1> Tu nombre eS : " + nombre + "</h1>");
+document.write("<h2> bienvenidos a js </h2>");
